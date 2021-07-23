643c7fba9266707c878f577c73adfa65545cb6a3
Denise <meagher.denise@gmail.com>
2021-07-23 16:41:35 -0700

    added popup light gallery for images

--- scripts.js
+++ scripts.js
@@ -12,3 +12,14 @@ $( document ).ready(function() {
     });
 
 });
+
+
+lightGallery(document.getElementById('lightgallery'), {
+	speed: 500,
+	download: false
+});
+
+lightGallery(document.getElementById('lightgallery-2'), {
+	speed: 500,
+	download: false
+});
